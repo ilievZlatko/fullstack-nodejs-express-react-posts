b1e2d13e1c74cc975b6da798ea4959fe3995ecc5
Zlatko Iliev <iliev.zlatko@gmail.com>
2019-01-03 11:21:40 +0200

    uploading files copleted

--- rest-api/app.js
+++ rest-api/app.js
@@ -1,16 +1,41 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const path = require('path')
+const path = require("path");
+const multer = require("multer");
 
 const feedRoutes = require("./routes/feed");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -31,7 +56,10 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect("mongodb://localhost:27017/postsdb", { useNewUrlParser: true })
+  .connect(
+    "mongodb://localhost:27017/postsdb",
+    { useNewUrlParser: true }
+  )
   .then(result => {
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
   })
