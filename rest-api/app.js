2c12e336f30cc33491969fab29ff47705fdb7642
Zlatko Iliev <iliev.zlatko@gmail.com>
2019-01-04 10:50:27 +0200

    fix relations between users and posts

--- rest-api/app.js
+++ rest-api/app.js
@@ -1,74 +1,74 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const path = require("path");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
 const fs = require('fs');
-const multer = require("multer");
+const multer = require('multer');
 
-const feedRoutes = require("./routes/feed");
-const authRoutes = require("./routes/auth");
+const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const imagesDir = path.join(__dirname, "images");
+const imagesDir = path.join(__dirname, 'images');
 
 const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + "-" + file.originalname);
-  }
+	destination: (req, file, cb) => {
+		cb(null, 'images');
+	},
+	filename: (req, file, cb) => {
+		cb(null, new Date().toISOString() + '-' + file.originalname);
+	}
 });
 
 const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype === "image/png" ||
-    file.mimetype === "image/jpg" ||
-    file.mimetype === "image/jpeg"
-  ) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
+	if (
+		file.mimetype === 'image/png' ||
+		file.mimetype === 'image/jpg' ||
+		file.mimetype === 'image/jpeg'
+	) {
+		cb(null, true);
+	} else {
+		cb(null, false);
+	}
 };
 
 if (!fs.existsSync(imagesDir)) {
-  fs.mkdirSync(imagesDir);
+	fs.mkdirSync(imagesDir);
 }
 
 app.use(bodyParser.json()); // application/json
 app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+	multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
 );
-app.use('/images', express.static(path.join(__dirname, "images")));
+app.use('/images', express.static(imagesDir));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+	);
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	next();
 });
 
-app.use("/feed", feedRoutes);
-app.use("/auth", authRoutes);
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => {
-  const status = error.statusCode || 500;
-  const message = error.message;
-  const data = error.data;
-  res.status(status).json({ message, data });
+	const status = error.statusCode || 500;
+	const message = error.message;
+	const data = error.data;
+	res.status(status).json({ message, data });
 });
 
 mongoose
-  .connect(
-    "mongodb://localhost:27017/postsdb",
-    { useNewUrlParser: true }
-  )
-  .then(result => {
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
-  })
-  .catch(err => console.log(err));
+	.connect(
+		'mongodb://localhost:27017/postsdb',
+		{ useNewUrlParser: true }
+	)
+	.then(result => {
+		app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+	})
+	.catch(err => console.log(err));
--- rest-api/routes/auth.js
+++ rest-api/routes/auth.js
@@ -6,27 +6,30 @@ const authController = require('../controllers/auth');
 
 const router = express.Router();
 
-router.put('/signup', [
-  body('email')
-    .isEmail()
-    .withMessage('Please enter a valid email.')
-    .custom((value, { req }) => {
-      return User.findOne({ email: value })
-        .then(userDoc => {
-          if (userDoc) {
-            return Promise.reject('email address already exists!');
-          }
-        });
-    })
-    .normalizeEmail({ gmail_remove_dots: false }),
-    body('password')
-      .trim()
-      .isLength({ min: 5 }),
-    body('name')
-      .trim()
-      .not()
-      .isEmpty()
-], authController.signup);
+router.put(
+	'/signup',
+	[
+		body('email')
+			.isEmail()
+			.withMessage('Please enter a valid email.')
+			.custom((value, { req }) => {
+				return User.findOne({ email: value }).then(userDoc => {
+					if (userDoc) {
+						return Promise.reject('email address already exists!');
+					}
+				});
+			})
+			.normalizeEmail({ gmail_remove_dots: false }),
+		body('password')
+			.trim()
+			.isLength({ min: 5 }),
+		body('name')
+			.trim()
+			.not()
+			.isEmpty()
+	],
+	authController.signup
+);
 
 router.post('/login', authController.login);
 
