87bedfe2933057302ba3cad26a68b6ce8c56eedf
Zlatko Iliev <iliev.zlatko@gmail.com>
2019-01-06 11:43:49 +0200

    fix bug with creating new post and set socket event for new created posts

--- rest-api/controllers/feed.js
+++ rest-api/controllers/feed.js
@@ -84,7 +84,7 @@ exports.createPost = (req, res, next) => {
 						...post._doc,
 						creator: {
 							_id: req.userId,
-							name: user.name
+							name: result.name
 						}
 					}
 				}
@@ -156,6 +156,7 @@ exports.updatePost = (req, res, next) => {
 	}
 
 	Post.findById(postId)
+		.populate('creator')
 		.then(post => {
 			if (!post) {
 				const error = new Error('Could not find post.');
@@ -163,7 +164,7 @@ exports.updatePost = (req, res, next) => {
 				throw error;
 			}
 
-			if (post.creator.toString() !== req.userId) {
+			if (post.creator._id.toString() !== req.userId) {
 				const error = new Error('Not authorized!');
 				error.statusCode = 403;
 				throw error;
@@ -179,6 +180,7 @@ exports.updatePost = (req, res, next) => {
 			return post.save();
 		})
 		.then(result => {
+			io.getIO().emit('posts', { action: 'update', post: result });
 			res.status(200).json({
 				message: 'Post updated!',
 				post: result
